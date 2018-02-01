571c7b9a669738da220c76f02a9ff77831a8a7da
JayLee <mjlee@loplat.com>
2018-02-01 17:25:33 +0900

    update a sample code

--- sample/app/src/main/java/com/loplat/loplatsample/MainActivity.java
+++ sample/app/src/main/java/com/loplat/loplatsample/MainActivity.java
@@ -1,31 +1,58 @@
 package com.loplat.loplatsample;
 
-import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.loplat.placeengine.PlaceEngine;
 import com.loplat.placeengine.Plengi;
 import com.loplat.placeengine.PlengiResponse;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     BroadcastReceiver mLoplatBroadcastReceiver;
     ProgressDialog mProgressDialog=null;
 
     private static final String PREFS_NAME = MainActivity.class.getSimpleName();
+
+    private static final int UPDATE_INTERVAL_MS = 1000;  // 1초
+    private static final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초
+    private static final int REQUEST_LOCATION_PERMISSION = 10000;
+    private static final int REQUEST_LOCATION_STATUS = 10001;
+
+    GoogleApiClient mGoogleApiClient;
+
+    LocationRequest locationRequest = new LocationRequest()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(UPDATE_INTERVAL_MS)
+            .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -192,46 +219,166 @@ public class MainActivity extends Activity {
         }
         else if(status == PlengiResponse.Result.FAIL_WIFI_SCAN_UNAVAILABLE) {
             // wifi scan is not available
+            checkWiFiScanCondition();
         }
     }
 
     public void onStopPlaceMonitoring(View view) {
         // request location to loplat engine
-        int result = Plengi.getInstance(this).stop();
-
-        if(result == PlengiResponse.Result.SUCCESS) {
-            // ok
-        }
-        else if(result == PlengiResponse.Result.FAIL_INTERNET_UNAVAILABLE) {
-            // internet is not connected
-        }
-        else if(result == PlengiResponse.Result.FAIL_WIFI_SCAN_UNAVAILABLE) {
-            // wifi scan is not available
-        }
+        Plengi.getInstance(this).stop();
     }
 
 
-    // Sample code for checking WiFi Scanning Condition in Mashmallow
-    private void checkWiFiScanConditionInMashmallow(Context context) {
+    // Sample code for checking WiFi Scanning Condition
+    private void checkWiFiScanCondition() {
 
-        if(Build.VERSION.SDK_INT < 23) {
-            return;
+        if (!checkLocationPermissionIfNeeded()) {
+            Toast.makeText(this, "Please grant a location permission", Toast.LENGTH_SHORT).show();
+        } else if (!checkGpsStatus()) {
+            Toast.makeText(this, "Please turn on GPS", Toast.LENGTH_SHORT).show();
+
+            /* GPS 켜는 방법은 google play service를 통해 앱내에서 직접 설정, GPS 설정화면 이용 등 2가지 방법이 있음
+               현재 sample code에서는 google play service를 이용하는 방법으로 작성되었음
+               google-play-service를 사용하지 않고 GPS 설정화면을 넘기는 경우에는 아래 주석처리된 코드 참조 바람
+
+               google play service(location) 사용하는 경우 -> dependency google-play-services 선언이 필요함
+               compile 'com.google.android.gms:play-services:[latest version]'
+            */
+            if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getApplicationContext()) == ConnectionResult.SUCCESS )
+            {
+                if (isGoogleClientConnected()) {
+                    turnGpsOnByGooglePlayService();
+                } else {
+                    connectGoogleClient();
+                }
+            }
+
+            // GPS 설정화면을 이용하는 방법
+            /*Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+            startActivity(intent);*/
+
+        } else if (!checkWifiScanIsAvailable()) {
+            // 안드로이드 4.3 이상 버전은 background wifi scan 설정이 켜져있으면 됨
+            // [참고: https://developer.android.com/reference/android/net/wifi/WifiManager.html#isScanAlwaysAvailable()]
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                Intent intent = new Intent(WifiManager.ACTION_REQUEST_SCAN_ALWAYS_AVAILABLE);
+                startActivity(intent);
+            } else {
+                // 안드로이드 4.2 이하 버전은 WiFi가 켜져있어야 wifi scanning 가능함
+                WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                wifiManager.setWifiEnabled(true);
+                Toast.makeText(this, "turn on WiFi", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void turnGpsOnByGooglePlayService(){
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(MainActivity.this, REQUEST_LOCATION_STATUS);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                }
+            }
+        });
+    }
+
+    private boolean checkWifiScanIsAvailable() {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        boolean wifiScanEnabled = false;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            wifiScanEnabled = wifiManager.isScanAlwaysAvailable();
         }
 
-        LocationManager locationManager = (LocationManager)context.getSystemService(context.LOCATION_SERVICE);
-        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        if(!isNetworkEnabled && !isGPSEnabled) {
-            // please turn on location settings
+        if(!wifiManager.isWifiEnabled() && !wifiScanEnabled){
+            return false;
         }
 
+        return true;
+    }
 
-        PackageManager pm = context.getPackageManager();
-        int permission = pm.checkPermission(Manifest.permission.ACCESS_FINE_LOCATION, context.getPackageName());
-        int subpermission = pm.checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION, context.getPackageName());
-        if(permission != PackageManager.PERMISSION_GRANTED && subpermission != PackageManager.PERMISSION_GRANTED) {
-            // please enable permission on location access
+    // check if gps is on
+    private boolean checkGpsStatus() {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
+            LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if(!isNetworkEnabled && !isGPSEnabled) {
+                // please turn on location settings
+                return false;
+            }
         }
+
+        return true;
+
+    }
+
+    // check if a location permission allowed
+    private boolean checkLocationPermissionIfNeeded() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        || shouldShowRequestPermissionRationale(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        || shouldShowRequestPermissionRationale(android.Manifest.permission.GET_ACCOUNTS)){
+                    //shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION) ) {
+                    // Explain to the user why we need to write the permission.
+                    //Toast.makeText(this, "Accept Permission", Toast.LENGTH_SHORT).show();
+                }
+
+
+                requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+                // MY_PERMISSION is an
+                // app-defined int constant
+                //}
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void connectGoogleClient() {
+        if (mGoogleApiClient == null ){
+            mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+
+        }
+        mGoogleApiClient.connect();
+    }
+
+    private boolean isGoogleClientConnected() {
+        if (mGoogleApiClient != null) {
+            if (mGoogleApiClient.isConnected() || mGoogleApiClient.isConnecting()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     // App에서 광고 연동 여부 설정
@@ -257,4 +404,19 @@ public class MainActivity extends Activity {
     }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // GPS 활성화
+        turnGpsOnByGooglePlayService();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
